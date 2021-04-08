20c755f284ac05c2244bba005c36eec7a0fd74d4
SimonePerucino <simone.perucino@gmail.com>
2021-04-08 07:00:46 +0200

    Esercizio Finito

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var nome = prompt("inserisci il tuo nome");
+var cognome = prompt("inserisci il tuo cognome");
+var colorePreferito = prompt("inserisci il tuo colore preferito");
+
+document.getElementById("input-utente").innerHTML = nome + cognome + colorePreferito ;
+
+console.log(nome+cognome+colorePreferito);
